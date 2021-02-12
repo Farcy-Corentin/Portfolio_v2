6d37d5edd1d1f7ab40bc4b798a8b6d87808fbaa0
Geoffrey Duplessi <grezor.gamer@gmail.com>
2021-02-12 17:47:56 +0100

    add layouts

--- resources/views/experiences.blade.php
+++ resources/views/experiences.blade.php
@@ -1,27 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+@section('content')
     experiences
     @foreach($experiences as $experience)
-        <h3>{{ $project->id }} === {{ $project->title }}</h3>
-        <p>{{ $project->description }} </p>
-        <ul>
-            <li>{{ $project->started_at }} - {{ $project->finished_at }}</li>
-        </ul>
-        <p>{{ $project->missions }} </p>
-        <p>{{ $project->languages }} </p>
-        <p>{{ $project->software }} </p>
-        <p>{{ $project->links }} </p>
-        <p>{{ $project->github_links }} </p>
-        <p>{{ $project->online }} </p>
-        <img src="{{ $project->pictures }}" alt="" srcset="">
-        <p>{{ }}</p>
+        <h3>{{ $experience->id }} === {{ $experience->title }}</h3>
     @endforeach
-</body>
-</html>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Portfolio</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/skills">Skills</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="/experiences">experiences</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="/projects">Projects</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="/contact">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container">
+ @yield('content')
+</div>
+
+@extends('layouts.footer')
