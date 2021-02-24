97a59129a27bc83538ac3600a1ed42b74ad2f150
Geoffrey Duplessi <grezor.gamer@gmail.com>
2021-02-24 15:37:41 +0100

    create route Project, insert database form

---
+++ resources/views/project/create.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+@section('title', '| Create New Project')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <h1>Create New Project</h1>
+            <hr>
+            {!! Form::open(['route' => 'project.store', 'method' => 'post', 'enctype' => 'multipart/form-data']) !!}
+                {{ Form::label('title', 'Title:') }}
+                {{ Form::text('title', null, ['class' => 'form-control']) }}
+
+                {{ Form::label('descriptions', 'Descriptions:') }}
+                {{ Form::textarea('descriptions', null, ['class' => 'form-control']) }}
+
+                {{ Form::label('started_at', 'Started_at:') }}
+                {{ Form::date('started_at', null, ['class' => 'form-control']) }}
+
+                {{ Form::label('finished_at', 'finished_at:') }}
+                {{ Form::date('finished_at', null, ['class' => 'form-control']) }}
+
+                {{ Form::label('missions', 'missions:') }}
+                {{ Form::textarea('missions', null, ['class' => 'form-control']) }}
+
+                {{ Form::label('languages', 'languages:') }}
+                {{ Form::text('languages', null, ['class' => 'form-control']) }}
+
+                {{ Form::label('software', 'software:') }}
+                {{ Form::textarea('software', null, ['class' => 'form-control']) }}
+
+                {{ Form::label('links', 'links:') }}
+                {{ Form::text('links', null, ['class' => 'form-control']) }}
+
+                {{ Form::label('github_links', 'github_links:') }}
+                {{ Form::text('github_links', null, ['class' => 'form-control']) }}
+
+                {{ Form::label('online', 'online:') }}
+                {{ Form::checkbox('online', 0, false) }}
+
+                {{ Form::label('pictures', 'pictures:') }}
+                {{ Form::text('pictures', null, ['class' => 'form-control']) }}
+
+                {{ Form::submit('Create Project', 
+                    [
+                        'class' => 'btn btn-success btn-lg btn-black',
+                        'style' => 'margin-top: 20px;'
+                    ])}}
+            {!! Form::close() !!}
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\SkillController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TrainingController;
+
 use App\Http\Controllers\ExperienceController;
+use App\Http\Controllers\Admin\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,23 +24,35 @@ use App\Http\Controllers\ExperienceController;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/skills', [SkillController::class , 'getIndex']);
 
-// Route::get('/trainings', function() {
-//     return view('trainings');
-// });
+Route::get('/trainings', function() {
+    return view('trainings');
+});
 
-// Route::get('/projects', function() {
-//     return view('projects');
-// });
+Route::get('/projects', function() {
+    return view('projects');
+});
+
 Route::get('/projects', [ProjectController::class , 'getProject']);
 
 Route::get('/experiences', [ExperienceController::class , 'getExperience']);
 
 Route::get('/trainings', [TrainingController::class , 'getTraining']);
 
-
 Route::get('/contact', function() {
     return view('contact');
+});
+
+/**
+ * Admin 
+ */
+Auth::routes();
+
+Route::resource('project', ProjectController::class);
+
+Route::prefix('admin')->name('admin.')->group(function(){
+     Route::resource('users', UsersController::class);
+     
 });
